fd6ea6295fc9ea129c905d637811c4c7b6b3daac
Balkishan <bkbajpay0609@gmail.com>
2025-03-16 11:53:30 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:5173",
+        origin: ["http://localhost:5173", "https://edu-nexus-flame.vercel.app"],
         credentials:true,
     })
 )
